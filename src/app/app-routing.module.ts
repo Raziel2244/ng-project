231884f21b056163b55a35543234ea8b1dda7e73
Elliot Thomas <raziel2244@pm.me>
2019-03-27 11:29:24 +0000

    App routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { RouterModule, Routes }  from '@angular/router';
 
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "shopping", loadChildren: "./shopping-list/shopping-list.module#ShoppingListModule" },
+  { path: "recipes", loadChildren: "./recipe-book/recipe-book.module#RecipeBookModule" },
+  { path:'', pathMatch: 'full', redirectTo: 'shopping' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
